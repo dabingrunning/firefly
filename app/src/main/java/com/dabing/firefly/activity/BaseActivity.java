5cb5f4bba8858d3e084f041109a9bfb6f069e463
dabing <hushaobing@imeyebang.com>
2017-11-04 11:07:59 +0800

    初始化创建项目

---
+++ app/src/main/java/com/dabing/firefly/activity/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.dabing.firefly.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dabing on 2017/11/4.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
---
+++ app/src/main/java/com/dabing/firefly/activity/welcome/WelcomeActivity.java
@@ -0,0 +1,17 @@
+package com.dabing.firefly.activity.welcome;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dabing on 2017/11/4.
+ */
+
+public class WelcomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
